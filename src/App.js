26aff91acd27ab2f8e3ebf5817f8b63438cd8823
Emily <eh1775@nyu.edu>
2019-05-20 16:42:58 +0800

    last changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
   
       this.state = {
           currentButton: "",
+          research: "", 
+          insights: "",
           isVisible: false, 
           drawingVisible: false
       };
@@ -18,19 +20,26 @@ class App extends Component {
     //functions go here
     aboutClicked = () => {
       this.setState({
-        currentButton: "I am a senior at NYU Shanghai studying Business and Marketing. Interested in DTC Brands, Fintech and Behavioral Economics",
+        currentButton: "I am a senior at NYU Shanghai studying Business and Marketing. I have worked on product for Smartbiz Loans and Zipsocket. Interested in DTC Brands, Fintech and Behavioral Economics",
         drawingVisible: false,
-        isVisible: false
+        isVisible: false,
+        research: "",
+        insights: ""
       });
     }
 
     projectClicked = () =>{
       this.setState ({
-        currentButton: 'My project is a model standard "Pick Three Lottery". It is desgined as a tool for researchers in behavioral economics and psychology on studying addiction. ' + 
-        "I conducted several user tests with individuals to understand their general experience using the App. " +
-        "After making adjustments based on this feedback I presented a paper drawing of the App. " +
-        "Through discussions I learned from users which features would drive continually engagement. " + 
-        "I also consulted articles on general psychology casino design including an article from theVerge and an article from the Telegraph",
+        currentButton: 'My project is a model of a standard "Pick Three Lottery". It is desgined as a tool for researchers in behavioral economics and psychology. ',
+
+        research: "I consulted articles on general psychology on casino design including an article from theVerge and an article from the Telegraph. " + 
+        "I presented several users a paper drawing of the application and asked them for general feedback on the overall design. " + 
+        "I then conducted some basic user testing with several users in order to test for engagement",
+
+        insights: "Through discussions I learned the decreasing the number of clicks the users increased engagement. " + 
+        "I also found that the design and feeling, particular flashy bright colors also drove engagement because it felt more fun and engaging for the users" + 
+        "Lastly, I also found that displaying the sequence of the house increase engagement because it encouraged people to get the number sequence the next time they played",
+
         drawingVisible: !this.state.drawingVisible,
         isVisible: false
       })
@@ -41,6 +50,8 @@ class App extends Component {
         isVisible: !this.state.isVisible,
         drawingVisible: false,
         currentButton: "Implemented is a keypad with numbers from 0 to 9. Once the participant selects three numbers and clicks play, the result is displayed with the randomly generated correct sequence, the number of points lost and a statement to get participants to play again",
+        research: "",
+        insights: ""
       })
     }
 
@@ -53,7 +64,9 @@ class App extends Component {
         "In order to for researchers this in a larger environment very quickly, I could put this on a blockchain and pay participants small amounst of cryptocurrency. " + 
         "It would also be helfpul to have a function that shows/hides a box that displays a number of points that a player starts with the test for differences in player behavior",
         drawingVisible: false,
-        isVisible: false
+        isVisible: false,
+        research: "",
+        insights: ""
       })
     }
   
@@ -64,21 +77,31 @@ class App extends Component {
         <h1>Hi! I'm Emily</h1>
           <div className="ButtonRow">
             <button value = {this.state.aboutButton} onClick={this.aboutClicked}>About Me</button>
-            <button onClick={this.projectClicked}>Project</button>
+            <button onClick={this.projectClicked}>Project</button> <br></br>
             <button onClick={this.prototypeClicked}>Prototype</button>
             <button onClick={this.futureClicked}>Future Work</button>
 
           </div>
           
           <div className="ResultBox">
-            <p>{this.state.aboutButton}</p>
+
+            <div className = "prototype">
             { this.state.isVisible ? (
                  <iframe src="https://giphy.com/embed/61Z51JR4SNysUVVdKN" width="480" height="252" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
             ) : null }
+            </div>
+
             <p className="result">{this.state.currentButton}</p>
+
+            <div className = "paperDrawing"> 
             { this.state.drawingVisible ? (
                  <img src= {drawing} width="480" height="252"></img>
             ) : null }
+            </div>
+
+            <p className="result">{this.state.research}</p>
+            <p className="result">{this.state.insights}</p>
+
           </div>
   
         </div>
